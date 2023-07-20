93afb6d841b958ddcc2a3878079fe066829af164
Jorge Luis Arteaga Almaraz <jorgitoaral@gmail.com>
2023-07-19 23:29:39 -0400

    build(rollup): improve ts build to include types

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,29 @@
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import del from 'rollup-plugin-delete';
 
-export default {
+export default [
+  {
     input: 'src/index.ts',
     output: {
-        dir: 'dist',
-        format: 'cjs'
+      dir: 'dist',
+      format: 'cjs'
     },
-    plugins: [typescript()]
-};
+    plugins: [
+      del({
+        targets: 'dist'
+      }),
+      typescript()
+    ]
+  },
+  {
+    input: 'dist/dts/index.d.ts',
+    output: [
+      {
+        file: 'dist/index.d.ts',
+        format: 'es'
+      }
+    ],
+    plugins: [dts()]
+  }
+];
